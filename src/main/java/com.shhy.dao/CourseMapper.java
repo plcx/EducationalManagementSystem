3925900128fe546d897fa15dd4cf3dfccb397cf5
win10 <758464172@qq.com>
2020-07-06 15:12:22 +0800

    7月6号上午

--- src/main/java/com.shhy.dao/CourseMapper.java
+++ src/main/java/com.shhy.dao/CourseMapper.java
@@ -8,7 +8,10 @@ public interface CourseMapper {
     Integer insert(Course course);//插入一条新的记录 insert into user (username,password) values (xxx,xxx)
     Integer delete(Integer cid);//删除一条记录  delete from user where id=xxx;
     Integer update(Course course);//更新一条记录  update user set username=xxxx,password=xxxxx,id=xxxx where id=xxx
-    List<Course> findAll();
+
+    static List<Course> findAll() {
+        return null;
+    }
 
     Course findOne(Integer id);//查找一条记录,   select * from user where id=xxx
 //    Student findOneByUser(Student user);//根据user对象的多个属性匹配,此例中用于登录检查
