d1fd98d7f3d3db27923594fa7960d7a9f688ac98
Nongweichang <824220377@qq.com>
2020-07-06 14:27:16 +0800

    最开始版本nwc

--- src/main/java/com.shhy.controller/TeacherController.java
+++ src/main/java/com.shhy.controller/TeacherController.java
@@ -1,4 +1,26 @@
 package com.shhy.controller;
 
+import com.shhy.domain.Teacher;
+import com.shhy.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/teacher")
 public class TeacherController {
+    @Autowired
+    private TeacherService teacherService;
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public ModelAndView findall(){
+        ModelAndView modelAndView =new ModelAndView("/teacher/list");
+        System.out.println("teacherController实现");
+        List<Teacher> teachers =teacherService.findAll();
+        modelAndView.addObject("teachers",teachers);
+        return modelAndView;
+    }
 }
--- src/main/java/com.shhy.dao/TeacherMapper.java
+++ src/main/java/com.shhy.dao/TeacherMapper.java
@@ -1,4 +1,14 @@
 package com.shhy.dao;
 
+import com.shhy.domain.Student;
+import com.shhy.domain.Teacher;
+
+import java.util.List;
+
 public interface TeacherMapper {
+    Integer insert(Teacher teacher);//插入一条新的记录 insert into user (username,password) values (xxx,xxx)
+    Integer delete(Integer id);//删除一条记录  delete from user where id=xxx;
+    Integer update(Teacher teacher);//更新一条记录  update user set username=xxxx,password=xxxxx,id=xxxx where id=xxx
+    List<Teacher> findAll();//查找所有记录   select * from user
+    Teacher findOne(Integer id);
 }
