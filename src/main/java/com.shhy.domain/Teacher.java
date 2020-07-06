1485865acd1add9b6c20f2bb1c032ae029a1b069
Commuovere <1484356946@qq.com>
2020-07-06 11:13:57 +0800

    最开始版本

--- src/main/java/com.shhy.domain/Teacher.java
+++ src/main/java/com.shhy.domain/Teacher.java
@@ -1,4 +1,9 @@
 package com.shhy.domain;
 
 public class Teacher {
+    private Integer tid;
+    private String password;
+    private String tname;
+    private String title;
+    private String tdepartment;
 }
