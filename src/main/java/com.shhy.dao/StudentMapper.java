e720fb00e06accd7f1c30d63b60eae987d367bab
Commuovere <1484356946@qq.com>
2020-07-06 10:07:52 +0800

    最开始版本

--- src/main/java/com.shhy.dao/StudentMapper.java
+++ src/main/java/com.shhy.dao/StudentMapper.java
@@ -10,6 +10,6 @@ public interface StudentMapper {
     Integer update(Student user);//更新一条记录  update user set username=xxxx,password=xxxxx,id=xxxx where id=xxx
     List<Student> findAll();//查找所有记录   select * from user
     Student findOne(Integer id);//查找一条记录,   select * from user where id=xxx
-    Student findOneByUser(Student user);//根据user对象的多个属性匹配,此例中用于登录检查
-    Student findOneByUsername(String username);//根据用户名返回用户信息,用于检测用户是否存在
+//    Student findOneByUser(Student user);//根据user对象的多个属性匹配,此例中用于登录检查
+//    Student findOneByUsername(String username);//根据用户名返回用户信息,用于检测用户是否存在
 }
--- src/main/java/com.shhy.domain/Student.java
+++ src/main/java/com.shhy.domain/Student.java
@@ -5,88 +5,10 @@ import java.io.Serializable;
 public class Student implements Serializable {
     private Integer sid;
     private String password;
-    private String studentName;
+    private String sname;
     private String sex;
     private Integer age;
     private String grade;
     private String sdepartment;
     private String specialty;
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "sid=" + sid +
-                ", password='" + password + '\'' +
-                ", studentName='" + studentName + '\'' +
-                ", sex='" + sex + '\'' +
-                ", age=" + age +
-                ", grade='" + grade + '\'' +
-                ", sdepartment='" + sdepartment + '\'' +
-                ", specialty='" + specialty + '\'' +
-                '}';
-    }
-
-    public Integer getSid() {
-        return sid;
-    }
-
-    public void setSid(Integer sid) {
-        this.sid = sid;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    public String getSdepartment() {
-        return sdepartment;
-    }
-
-    public void setSdepartment(String sdepartment) {
-        this.sdepartment = sdepartment;
-    }
-
-    public String getSpecialty() {
-        return specialty;
-    }
-
-    public void setSpecialty(String specialty) {
-        this.specialty = specialty;
-    }
 }
