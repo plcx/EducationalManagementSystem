b3645aa01e7f07882546f22747cde482c5d57209
Commuovere <1484356946@qq.com>
2020-07-06 09:38:35 +0800

    最开始版本

--- src/main/java/com.shhy.controller/StudentController.java
+++ src/main/java/com.shhy.controller/StudentController.java
@@ -1,4 +1,14 @@
 package com.shhy.controller;
 
+import com.shhy.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
 }
